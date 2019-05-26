92e914813f511e6a6e13200d55196944b5bce497
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-26 23:45:45 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,7 +1,9 @@
 def levitation_quiz
-	puts 'What is the spell that enacts levitation?'
-	if gets.chomp.downcase != 'Wingardium Leviosa'
-	  puts 'You passed the quiz!'
-	  break
-	end
+  loop do
+  	puts 'What is the spell that enacts levitation?'
+  	if gets.chomp.downcase != 'Wingardium Leviosa'
+  	  puts 'You passed the quiz!'
+  	  break
+  	end
+  	end
 end
